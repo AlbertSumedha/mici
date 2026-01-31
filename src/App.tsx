6ae1ae88a31f24b0639cd4b89c376f99c03e9eac
AlbertSumedha <albertsk2581@gmail.com>
2026-01-31 12:27:10 +0700

    cleanup: use main.tsx and remove duplicate jsx files

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // Ganti import Index dengan import file Valentine kamu
-import ValentinePage from "./Apps.jsx"; 
+import ValentinePage from "./ValentinePage.tsx"; 
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
